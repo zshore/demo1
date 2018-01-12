dda0669baef0701cabbf75c7f56d9f022cb11be5
zs522008 <zs522008@163.com>
2018-01-12 16:41:37 +0800

    第二提交代码.修改了Restcontroller

--- src/main/java/com/shore/web/HomeController.java
+++ src/main/java/com/shore/web/HomeController.java
@@ -3,19 +3,17 @@ package com.shore.web;
 import com.shore.entity.Product;
 import com.shore.service.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-@Controller
+@RestController
 @RequestMapping("/home")
 public class HomeController {
     @Autowired
     private HomeService homeService;
 
-    @ResponseBody
+   // @ResponseBody
     @RequestMapping("/index")
     public Object  index(){
         List<Product> allProducts = homeService.findAllProducts();
